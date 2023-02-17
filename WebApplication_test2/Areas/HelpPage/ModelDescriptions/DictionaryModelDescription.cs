0ecbfb50b08af4cd92b3520fc42795fcd69c4563
MikeWyatt1966 <mike_wyatt@hotmail.com>
2023-02-17 11:59:43 +0100

    Add project files.

---
+++ WebApplication_test2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplication_test2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApplication_test2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_test2.MvcApplication" Language="C#" %>
